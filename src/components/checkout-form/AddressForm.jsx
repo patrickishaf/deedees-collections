8c962c70f2d66a262c2447d6ad5ad902a8c396b1
onumdev <onumdev@gmail.com>
2021-07-03 17:04:24 +0100

    fetch the shipping subdivisions in componentDidMount

--- src/components/checkout-form/AddressForm.jsx
+++ src/components/checkout-form/AddressForm.jsx
@@ -6,26 +6,44 @@ import { commerce } from '../../lib/commerce';
 
 const AddressForm = ({ checkoutToken }) => {
     const [shippingCountries, setShippingCountries] = useState([]);
-    const [selectedCountry, setSelectedCountry] = useState("");
+    const [shippingCountry, setShippingCountry] = useState("");
     const [shippingSubdivisions, setShippingSubdivisions] = useState([]);
-    const [selectedSubdivision, setSelectedSubdivision] = useState("");
+    const [shippingSubdivision, setShippingSubdivision] = useState("");
     const [shippingOptions, setShippingOptions] = useState([]);
-    const [selectedOption, setSelectedOption] = useState();
+    const [shippingOption, setShippingOption] = useState();
     const methods = useForm();
 
-    const countries = Object.entries(shippingCountries.map(([code, name]) => ({id: code, label: name})))
+    const countries = Object.entries(shippingCountries.map(([code, name]) => ({id: code, label: name})));
+    const subdivisions = Object.entries(shippingSubdivisions.map(([code, name]) => ({id: code, label: name})));
+    console.log("YOUR COUNTRIES ARE: ");
+    console.log(countries);
 
     const fetchShippingCountries = async (checkoutTokenId) => {
-        const { countries } = await commerce.services.localeListShippingCountries(checkoutTokenId);
-        console.log("YOUR SHIPPING COUNTRIES ARE:");
+        const { countries } = await commerce.services.localeListShippingCountries();
+        console.log("THE COUNTRIES FETCHED FROM THE BACKEND ARE: ");
         console.log(countries);
         setShippingCountries(countries);
-        setSelectedCountry(Object.keys(countries)[0]);
+        setShippingCountry(Object.keys(countries)[0]);
+    }
+
+    const fetchSubdivisions = async (countryCode) => {
+        const { subdivisions } = await commerce.services.localeListSubdivisions(countryCode);
+        console.log("THE SUBDIVISIONS FETCHED FROM THE BACKEND ARE: ");
+        console.log(subdivisions);
+
+        setShippingSubdivisions(subdivisions);
+        setShippingSubdivision(Object.keys(subdivisions)[0]);
     }
 
     useEffect(() => {
         fetchShippingCountries(checkoutToken.id);
     }, []);
+
+    useEffect(() => {
+        console.log("YOUR SHIPPING COUNTRY'S CODE IS: ");
+        console.log(shippingCountry.code);
+        if (shippingCountry) fetchSubdivisions(shippingCountry.code);
+    }, [shippingCountry]);
 
     return (
         <>
@@ -41,10 +59,12 @@ const AddressForm = ({ checkoutToken }) => {
                         <FormInput required name="Zip" label="Postal code" />
                         <Grid item xs={12} sm={6}>
                             <InputLabel>Shipping Country</InputLabel>
-                            <Select value={selectedCountry} fullWidth onChanged={(e)=>setSelectedCountry(e.target.value)}>
-                                <MenuItem key="" value="">
-                                    Select Me
-                                </MenuItem>
+                            <Select value={shippingCountry} fullWidth onChanged={(e)=>setShippingCountry(e.target.value)}>
+                                {countries.map((country) => (
+                                    <MenuItem key={country.id} value={country.id}>
+                                        {country.label}
+                                    </MenuItem>
+                                ))}
                             </Select>
                         </Grid>
                         {/*<Grid item xs={12} sm={6}>
